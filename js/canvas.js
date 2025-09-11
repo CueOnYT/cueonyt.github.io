8ea1b57113beabdf42f9bae151a73d13bc64957a
CueOnYT <29dslifer@usd339.net>
2025-09-11 08:55:37 -0500

    Update canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -1,1 +1,26 @@
+const Canvas = {
+  init() {
+    this.canvas = document.getElementById("yearbookCanvas");
+    this.ctx = this.canvas.getContext("2d");
 
+    this.canvas.addEventListener("dblclick", e => {
+      const text = prompt("Enter text:");
+      if (text && Core.currentProject) {
+        Core.currentProject.pages[Core.currentProject.currentPage || 0].elements.push({
+          type: "text",
+          text,
+          x: e.offsetX,
+          y: e.offsetY,
+          size: 20,
+          color: "#fff",
+          font: "Arial"
+        });
+        Core.renderPage();
+      }
+    });
+  }
+};
+
+window.onload = () => {
+  Canvas.init();
+};
